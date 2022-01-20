adba584a80a10ec10b49d196864a5e840207b469
Lucas Oi <lucas.oi-@hotmail.com>
2022-01-19 23:30:21 -0300

    Atulualização com inclusão dos metodos

--- blogpessoal/src/main/java/br/org/generation/blogpessoal/repository/PostagemRepository.java
+++ blogpessoal/src/main/java/br/org/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,5 +1,7 @@
 package br.org.generation.blogpessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,7 @@ import br.org.generation.blogpessoal.model.Postagem;
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
 	
-
+	List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
+	
+	/* select * from tb_postagens where titulo like "%titulo%";  */
 }
